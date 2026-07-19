f818eee476e9f3265ffc737c1fd2af5b8ecdc883
SiaSheerland <x1075956441x@163.com>
2026-07-20 05:13:11 +0800

    perf: increase async IO batch size from 100 to 16384

--- src/io/async_io/aio_context.h
+++ src/io/async_io/aio_context.h
@@ -75,7 +75,7 @@ public:
 
 public:
     /// Default number of concurrent AIO requests supported.
-    static constexpr int64_t DEFAULT_REQUEST_COUNT = 100;
+    static constexpr int64_t DEFAULT_REQUEST_COUNT = 16384;
 
     /// The libaio context handle.
     io_context_t ctx_;
